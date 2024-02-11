843d11005233e286fa5c8f4435dd9fd023a4a75c
M0hammadRokib <rokib.solvrz@gmail.com>
2024-02-11 15:16:55 +0600

    starting cotest 1928

---
+++ 1928/1928B.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    freopen("../input.txt", "r", stdin);
+
+    int t;
+    scanf("%d", &t);
+
+    while(t--) {
+        //
+    }
+    return 0;
+}
