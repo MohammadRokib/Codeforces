d0aefa8124cd742fb2380816b6e0de41fda1523e
M0hammadRokib <rokib.solvrz@gmail.com>
2024-02-13 09:32:41 +0600

    completed 1928B Accepted

--- 1928/1928B.cpp
+++ 1928/1928B.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <string>
 using namespace std;
 
 int main() {
@@ -8,7 +9,24 @@ int main() {
     scanf("%d", &t);
 
     while(t--) {
-        //
+        int n;
+        scanf("%d", &n);
+
+        int ara[n];
+        for(int i=0; i<n; i++)
+            scanf("%d", &ara[i]);
+        sort(ara, ara+n);
+
+        int p = 0;
+        for (int i=1; i<n; i++)
+            if (ara[p] != ara[i]) swap(ara[++p], ara[i]);
+
+        int ans = 1, k = 0;
+        for (int i=1; i<=p; i++) {
+            if (ara[i]-ara[k] < n) ans = max(ans, i-k+1);
+            else k++;
+        }
+        printf("%d\n", ans);
     }
     return 0;
 }
